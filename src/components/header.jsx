1d62c0c5e87573700a71ac70249477541230bf4c
Jared Young <jyoung10078@gmail.com>
2025-03-07 22:49:18 -0600

    Header Prototype

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import logo from '../assets/claude-travel-logo.png'
 export default function Header() {
     return (
         <header>
-            <h1>Header</h1>
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <h1>Travel Consultant Claude</h1>
         </header>
     )
 }

30b8571e872ff556e09f4dc96103003273ae6136
Jared Young <79735672+jyoung10078@users.noreply.github.com>
2025-03-09 23:04:16 -0500

    Making Mobile Friendly

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -35,9 +35,18 @@ export default function Body() {
     return (
         <main>
             <form action={handleAction} className="input-container">
-                <input type="text" name="destination" placeholder={destination ? destination : "Where are you heading?"} />
-                <input type="text" name="length-of-stay" placeholder={lengthOfStay ? lengthOfStay : "How long are you staying?"} />
-                <input type="text" name="attraction" placeholder={attactions ? attactions : "Anything you want to see/do?"} />
+                <div className="input-container-form-group">
+                    <label htmlFor="destination">Destination</label>    
+                    <input type="text" name="destination" placeholder={destination ? destination : "Where are you heading?"} />
+                </div>
+                <div className="input-container-form-group">
+                    <label htmlFor="length-of-stay">Length of Stay</label>
+                    <input type="text" name="length-of-stay" placeholder={lengthOfStay ? lengthOfStay : "How long are you staying?"} />
+                </div>
+                <div className="input-container-form-group">
+                    <label htmlFor="attraction">Attractions</label>
+                    <input type="text" name="attraction" placeholder={attactions ? attactions : "Anything you want to see/do?"} />
+                </div>
                 <button type="submit">Enter</button>
                 <button type="button" onClick={handleClear}>Clear</button>
             </form>
